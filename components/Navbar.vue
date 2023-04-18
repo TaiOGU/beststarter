589583a99fff5a6d9762b7c5fcdb69e03eb7f149
Tai <jaku@riseup.net>
2023-04-18 17:08:35 +0100

    Added default.vue and changed the main page system

---
+++ components/Navbar.vue
@@ -0,0 +1,3 @@
+<template>
+<h1 class="font-sans">Navbar here</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1 class="font-mono">Welcome to BestStarter!</h1>
+        <p>A seamless way to start your Nuxt project!</p>
+    </div>
+</template>
